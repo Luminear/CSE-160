a8c70af34847eace4afa03c61ed1daa07cd777b2
Luminear <spruitens@gmail.com>
2026-01-10 17:01:31 -0800

    step 3 finished

--- asgn0/asg0.js
+++ asgn0/asg0.js
@@ -20,13 +20,24 @@ function drawVector(v, color) {
 
     ctx.strokeStyle = color;
 
-    // console.log("you got this far");
-    // console.log(v.elements[0]);
-
     let cx = canvas.width/2;
     let cy = canvas.height/2;
     ctx.beginPath();
     ctx.moveTo(cx, cy);
     ctx.lineTo(cx + (v.elements[0] * 20), cy - (v.elements[1] * 20));
     ctx.stroke();
+}
+
+function handleDrawEvent() {
+    var canvas = document.getElementById('example');
+    var ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = 'rgba(0, 0, 0, 1.0)';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    let v1x = document.getElementById("v1x").value;
+    let v1y = document.getElementById("v1y").value;
+
+    const v1 = new Vector3([v1x, v1y, 0]);
+    drawVector(v1, "red");
 }

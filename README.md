89fe2249de6613e251946bcaed7de7fabad11ad3
Luminear <spruitens@gmail.com>
2026-01-16 21:45:19 -0800

    asgn 1 step 5 finished

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Base: https://luminear.github.io/CSE-160/
 
 asgn0: https://luminear.github.io/CSE-160/asgn0/asg0.html
+
+asgn1: https://luminear.github.io/CSE-160/asgn1/asgn1.html
--- asgn1/asgn1.js
+++ asgn1/asgn1.js
@@ -2,9 +2,10 @@
 // Vertex shader program
 var VSHADER_SOURCE = `
   attribute vec4 a_Position;
+  uniform float u_Size;
   void main() {
     gl_Position = a_Position;
-    gl_PointSize = 10.0;
+    gl_PointSize = u_Size;
   }`
 
 // Fragment shader program
@@ -19,6 +20,7 @@ let canvas;
 let gl;
 let a_Position;
 let u_FragColor;
+let u_Size;
 
 function main() {
 
@@ -38,6 +40,7 @@ function main() {
 }
 
 let g_selectedColor = [1.0, 1.0, 1.0, 1.0];
+let g_selectedSize = 5;
 
 function actionsForHTMLUI() {
   document.getElementById('green').onclick = function() {g_selectedColor = [0.0, 1.0, 0.0, 1.0];};
@@ -46,10 +49,13 @@ function actionsForHTMLUI() {
   document.getElementById('redSlide').addEventListener('mouseup', function() {g_selectedColor[0] = this.value/100;});
   document.getElementById('greenSlide').addEventListener('mouseup', function() {g_selectedColor[1] = this.value/100;});
   document.getElementById('blueSlide').addEventListener('mouseup', function() {g_selectedColor[2] = this.value/100;});
+
+  document.getElementById('sizeSlide').addEventListener('mouseup', function() {g_selectedSize = this.value;});
 }
 
 var g_points = [];  // The array for the position of a mouse press
 var g_colors = [];  // The array to store the color of a point
+var g_sizes = [];
 
 function click(ev) {
 
@@ -58,6 +64,7 @@ function click(ev) {
   // Store the coordinates to g_points array
   g_points.push([x, y]);
   g_colors.push(g_selectedColor.slice());
+  g_sizes.push(g_selectedSize);
   
   // Store the coordinates to g_points array
   // if (x >= 0.0 && y >= 0.0) {      // First quadrant
@@ -103,6 +110,12 @@ function connectVariablesToGLSL() {
     console.log('Failed to get the storage location of u_FragColor');
     return;
   }
+
+  u_Size = gl.getUniformLocation(gl.program, 'u_Size');
+  if (!u_Size) {
+    console.log('Failed to get the storage location of u_Size');
+    return;
+  }
 }
 
 function renderAllShapes() {
@@ -113,11 +126,14 @@ function renderAllShapes() {
   for (var i = 0; i < len; i++) {
     var xy = g_points[i];
     var rgba = g_colors[i];
+    var size = g_sizes[i];
 
     // Pass the position of a point to a_Position variable
     gl.vertexAttrib3f(a_Position, xy[0], xy[1], 0.0);
     // Pass the color of a point to u_FragColor variable
     gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
+    // Pass the size of a point
+    gl.uniform1f(u_Size, size);
     // Draw
     gl.drawArrays(gl.POINTS, 0, 1);
   }
